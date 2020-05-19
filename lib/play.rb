547cae6336ed09b8e12c253b2f443bdcb5c61032
Ashleigh Petty <petty.ashleigh@yahoo.com>
2020-05-19 20:15:46 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -36,9 +36,12 @@ def turn(board)
 end
 
 # define your play method below
-def play(board)
-  turn = 0
-  while turn < 9
-    turn += 1
-  end
-  end
+def turn_count(board)
+   counter = 0
+   board.each do |spaces|
+      if spaces == "X" || spaces == "O"
+         counter += 1
+      end
+   end
+   counter
+end
